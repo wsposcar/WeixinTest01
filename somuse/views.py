f62b63964ff71e9d0d91715f8851df53411ec2c5
wsposcar <oscar.wangsp@gmail.com>
2023-01-30 23:05:21 +0800

    added auth

--- somuse/views.py
+++ somuse/views.py
@@ -3,6 +3,7 @@ from somuse import app
 from flask import render_template, request
 import requests
 
+
 @app.route('/')
 def index():
     return render_template('index.html', user='user', super_user=False)
@@ -11,10 +12,19 @@ def index():
 @app.route('/fullchain', methods=['GET', 'POST'])
 def fullchain():
     lyrics = request.form.get('text')
-    x = requests.post('http://somuse.asuscomm.com:88/fullchain', {'text': lyrics, 'ai': 'true', 'gender':'female'})
-    return 'http://somuse.asuscomm.com:88/' + x.text
+    x = requests.post('http://somuse.asuscomm.com:88/fullchain',
+                      {'text': lyrics, 'ai': 'true', 'gender': 'female'})
+    return 'https://somuse.asuscomm.com:88/' + x.text
+
 
 @app.route('/lyric', methods=['GET'])
 def lyric():
     x = requests.get('http://somuse.asuscomm.com:88/lyric')
     return x.text
+
+
+@app.route('/auth', methods=['GET'])
+def auth():
+    x = request.get(
+        'GET https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code')
+    return x.json
