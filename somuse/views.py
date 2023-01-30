ab2fbf8cb9123abace6e38ca864b26167b54dc7f
wsposcar <oscar.wangsp@gmail.com>
2023-01-30 23:39:29 +0800

    added msgCheck

--- somuse/views.py
+++ somuse/views.py
@@ -26,6 +26,24 @@ def lyric():
 @app.route('/auth', methods=['GET'])
 def auth():
     code = request.form.get('code')
-    x = request.get(
-        'GET https://api.weixin.qq.com/sns/jscode2session?appid=wx8d60bfc02a9179c2&secret=8fccb11216c2ef6c72711111b2163a19&js_code=' + code + '&grant_type=authorization_code')
+    x = requests.get(
+        'https://api.weixin.qq.com/sns/jscode2session?appid=wx8d60bfc02a9179c2&secret=8fccb11216c2ef6c72711111b2163a19&js_code=' + code + '&grant_type=authorization_code')
+    return x.json
+
+
+@app.route('/msgCheck', methods=['POST'])
+def auth():
+    access = requests.get(
+        'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx8d60bfc02a9179c2&secret=8fccb11216c2ef6c72711111b2163a19')
+    access_token = access.json['access_token']
+    openid = request.form.get('openid')
+    content = request.form.get('content')
+    data = {
+        "openid": openid,
+        "scene": 1,
+        "version": 2,
+        "content": content
+    }
+    x = requests.post(
+        'https://api.weixin.qq.com/wxa/msg_sec_check?access_token='+access_token, data=data)
     return x.json
